20b2af4685fe2fb8a0684a439f6b8fd386b9a2a3
eltoncelestino <eltongf2014@gmail.com>
2022-04-06 15:51:08 -0300

    Configuração da aplicação

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function Button() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <button onClick={increment}>{counter}</button>
+  )
+}
